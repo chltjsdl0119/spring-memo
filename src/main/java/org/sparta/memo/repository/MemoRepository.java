c68dd788d5fed3db07da57d406c04cc067d4bb31
Seungwon_Choi <bestwondev@gmail.com>
2024-07-02 13:25:18 +0900

    Query Methods

--- src/main/java/org/sparta/memo/repository/MemoRepository.java
+++ src/main/java/org/sparta/memo/repository/MemoRepository.java
@@ -3,6 +3,9 @@ package org.sparta.memo.repository;
 import org.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemoRepository extends JpaRepository<Memo, Long> {
+import java.util.List;
 
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByUsername(String username);
 }
